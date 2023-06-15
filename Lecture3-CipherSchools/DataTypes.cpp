ac82b891cf60692c33e317ca0a00b183d2cd7345
Munish <136222391+Munish1734@users.noreply.github.com>
2023-06-15 17:56:45 +0530

    Add files via upload

---
+++ Lecture3-CipherSchools/DataTypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=3;
+    char c='x';
+    float f=34.2;
+    bool b=true;
+    cout<<n<<endl<<c<<endl<<f<<endl<<b<<endl;
+    return 0;
+} 
---
+++ Lecture3-CipherSchools/variables_constants_dataTypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5,m=6;//these are constants
+    cout<<n<<m<<endl;
+
+    int N,M;
+    cin>>N>>M;//these are variables
+    cout<<N<<M;
+    return 0;
+}
