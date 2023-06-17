cc6da3083d4e554a355db94d24f2b59232ab49b1
Munish <136222391+Munish1734@users.noreply.github.com>
2023-06-17 11:44:01 +0530

    Add files via upload

---
+++ Lecture3 II-CipherSchools/DataTypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x;
+    int y;
+
+    x=89;
+    y=234;
+
+    int c = x+y;
+
+    cout<<x<<endl<<y<<endl<<c;
+
+    return 0;
+}
---
+++ Lecture3 II-CipherSchools/Endl.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"this is usage of endl"<<endl<<"this line will be printed in next line because we used endl here";
+    return 0;
+}
