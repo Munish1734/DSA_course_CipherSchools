e1e8eb6fa26a3907a8ac1a6b96d48979214f14b6
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-04 17:46:32 +0530

    Add files via upload

---
+++ Lecture5 I-CipherSchools/Basic_Arrays.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;//this can store only 1 values
+    int a[5];//this became an array which can store 5 elements
+    a[0]=72;
+    a[1]=12;
+    a[2]=52;
+    a[3]=42;
+    a[4]=42;
+    cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3]<<" "<<a[4];
+    return 0;
+}
