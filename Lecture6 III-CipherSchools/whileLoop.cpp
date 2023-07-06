c1ba7e96d626e224dc2ddb930ca706d04ab5a875
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-07 00:01:51 +0530

    Add files via upload

---
+++ Lecture6 III-CipherSchools/whileLoop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    //WHILE
+    int m , i = 0;
+    cin>>m;
+    while(i < m){
+        cout<<"this work is being done by : "<<i<<" people till now"<<endl;
+        i++;
+    }
+    return 0;
+}
