0388fd2d8e5634cd7a8f023b788e02fc4103e97a
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-08 16:43:07 +0530

    Add files via upload

---
+++ Lecture8 I -CipherSchools/Functions_part1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int sum(int a, int b){//this is sum function
+        return (a+b);
+    }
+int main(){//this is main function
+    cout<<sum(23,45);
+    return 0;
+}
