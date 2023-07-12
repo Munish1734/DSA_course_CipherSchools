9e42277e996d410d2d6608bfeb934f21e71b11c9
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-12 09:29:00 +0530

    Add files via upload

---
+++ Lecture11 II -CipherSchools/Address_of_elements.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    long int m;
+    double o;
+    cout<<"size of respective variables is : "<<" "<<sizeof(n)<<" "<<sizeof(m)<<" "<<sizeof(o)<<" "<<endl;
+    cout<<"address of respective variables is : "<<" "<<&n<<" "<<&m<<" "<<&o<<endl;
+    return 0;
+}
