4be1cfe1b8b67af971d127a5cd920f135aa7ab02
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-18 22:17:52 +0530

    Add files via upload

---
+++ Lecture16  -CipherSchools/Introduction_to_algorithms.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //algorithm is logical thinking/solution to proceed towards a problem
+    int n =23;
+    int x = n*4;
+    int y = n+n+n+n;
+    cout<<x<<endl<<y;//both will give same solution but both have there different procedure
+    // X is much efficient
+    return 0;
+}
