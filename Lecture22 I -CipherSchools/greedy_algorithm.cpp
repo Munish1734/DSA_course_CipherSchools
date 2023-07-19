edd32422dd3f21cd9d6810584efecb4df98a6f21
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-19 12:00:32 +0530

    Add files via upload

---
+++ Lecture22 I -CipherSchools/greedy_algorithm.cpp
@@ -0,0 +1,3 @@
+// A greedy algorithm is an approach for solving a problem by selecting the best option available at the moment.
+// It doesn't worry whether the current best result will bring the overall optimal result.
+// The algorithm never reverses the earlier decision even if the choice is wrong.
