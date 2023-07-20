41b99f34931bebf75c5bfeed0efc3da3046916f8
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-20 14:17:08 +0530

    Add files via upload

---
+++ Lecture25 II -CipherSchools/classes_part2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    public :
+    string name;
+    int mids ;
+    float ends;
+    bool present_absent;
+};
+
+class{
+    string car_name;
+    string tyre;
+    int no_of_tyres;
+    int no_of_seats;
+};
+
+int main(){
+
+    return 0;
+}
