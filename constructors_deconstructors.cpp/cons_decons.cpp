4dcc436c85e25320738a94af53665457a835aed9
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-24 16:28:44 +0530

    Add files via upload

---
+++ constructors_deconstructors.cpp/cons_decons.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+ 
+class Z
+{
+public:
+    // constructor
+    Z()
+    {
+        cout<<"Constructor called"<<endl;
+    }
+ 
+    // destructor
+    ~Z()
+    {
+        cout<<"Destructor called"<<endl;
+    }
+};
+ 
+int main()
+{
+    Z z1;   // Constructor Called
+    int a = 1;
+    if(a==1)
+    {
+        Z z2;  // Constructor Called
+    }  // Destructor Called for z2
+} //  Destructor called for z1
