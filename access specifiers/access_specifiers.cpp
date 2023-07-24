1ec37b7bb5e6373031906772c70edab6e2cc6e4e
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-24 15:49:38 +0530

    Add files via upload

---
+++ access specifiers/access_specifiers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+class MyClass {
+  public:    // Public access specifier
+    int x;   // Public attribute
+  private:   // Private access specifier
+    int y;   // Private attribute
+};
+
+int main() {
+  MyClass myObj;
+  myObj.x = 25;  // Allowed (public)
+  myObj.y = 50;  // Not allowed (private)
+  return 0;
+}
