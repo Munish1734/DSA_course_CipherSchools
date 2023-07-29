ed869c929b3766c748f3e6dfc3ab56e56d040f3d
Munish <136222391+Munish1734@users.noreply.github.com>
2023-07-29 11:45:27 +0530

    Add files via upload

---
+++ Lecture31 I -CipherSchools/merge_sort_part1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Merge function to merge two sorted arrays
+void merge(int arr[], int left[], int leftSize, int right[], int rightSize) {
+    int i = 0, j = 0, k = 0;
+    while (i < leftSize && j < rightSize) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
+    }
+    while (i < leftSize) {
+        arr[k++] = left[i++];
+    }
+    while (j < rightSize) {
+        arr[k++] = right[j++];
+    }
+}
+//this is simple merge sorting function
